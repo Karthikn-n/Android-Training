a951275fed6ae55e5115aaa8de36b07db9bd504d
Karthikn-n <karthikn11092001@gmail.com>
2025-07-09 19:14:25 +0530

    Added lemonade

---
+++ app/src/main/java/com/example/happybirthday/basics/Lemonade.kt
@@ -0,0 +1,10 @@
+package com.example.happybirthday.basics
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import java.lang.reflect.Modifier
+
+@Composable
+fun Lemonade(modifier: Modifier, navController: NavHostController) {
+
+}
